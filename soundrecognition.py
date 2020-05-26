88e74c6147a341787e08d07d51ad745bae42de0b
sancia15 <59640268+sancia15@users.noreply.github.com>
2020-05-26 12:42:26 +0530

    Create soundrecognition.py

---
+++ soundrecognition.py
@@ -0,0 +1,42 @@
+import speech_recognition as sr
+import webbrowser as wb
+
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('[search video: search youtube]')
+    print('speak now')
+    audio=r3.listen(source)
+
+if 'edureka' in r2.recognize_google(audio):
+    r1 = sr.Recognizer()
+    url='https://www.edureka.co/'
+    with sr.Microphone() as source:
+        print('search your query')
+        audio = r2.listen(source)
+
+        try:
+            get = r2.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError:
+            print('error')
+        except sr.RequestError as e:
+            print('failed'.format(e))
+if 'video' in r1.recognize_google(audio):
+    r1 = sr.Recognizer()
+    url='https://www.youtube.com/results?search_query='
+    with sr.Microphone() as source:
+        print('search your query')
+        audio = r1.listen(source)
+
+        try:
+            get = r1.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError:
+            print('error')
+        except sr.RequestError as e:
+            print('failed'.format(e))
